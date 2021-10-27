cbf6886113b3318d1f6d1e6284f6b57713aa13fc
david <david@kattfest.se>
2021-10-28 00:06:40 +0200

    prettier

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,33 @@
 class Game {
-  ctx: CanvasRenderingContext2D
+  ctx: CanvasRenderingContext2D;
   constructor() {
     const canvas = document.getElementById("Game") as HTMLCanvasElement;
-    this.ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext("2d");
   }
 
-  start() { this.drawBorder(); }
+  start() {
+    this.drawBorder();
+  }
 
   drawBorder(width = 12, height = 22) {
-    let border_blocks: Block[] = [];
+    const border_blocks: Block[] = [];
     for (let ix = 0; ix < width; ix++) {
       for (let iy = 0; iy < height; iy++) {
         if (iy == 0 || iy == height - 1 || ix == 0 || ix == width - 1) {
-          border_blocks.push(new Block(ix + 1, iy + 1, "grey"))
+          border_blocks.push(new Block(ix + 1, iy + 1, "grey"));
         }
       }
     }
-    border_blocks.forEach(b => b.draw());
+    border_blocks.forEach((b) => b.draw());
   }
 }
 
 class Block extends Game {
-  size = 20
-  margin = 2
-  pos_x: number
-  pos_y: number
-  color: string
+  size = 20;
+  margin = 2;
+  pos_x: number;
+  pos_y: number;
+  color: string;
 
   constructor(pos_x: number, pos_y: number, color: string) {
     super();
@@ -36,11 +38,21 @@ class Block extends Game {
 
   draw() {
     this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(this.pos_x * (this.size + this.margin),
-                      this.pos_y * (this.size + this.margin), this.size,
-                      this.size);
+    this.ctx.fillRect(
+      this.pos_x * (this.size + this.margin),
+      this.pos_y * (this.size + this.margin),
+      this.size,
+      this.size
+    );
   }
 }
 
-const game = new Game()
-game.start()
+class Tetromino extends Block {
+  // prettier-ignore
+  straight = [0, 0, 0,
+              0, 0, 0,
+              1, 1, 1];
+}
+
+const game = new Game();
+game.start();
