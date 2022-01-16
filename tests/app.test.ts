f28d9d3cf5e9ba37292cdb56b716c943615f7fb5
david <david@kattfest.se>
2022-01-16 10:44:49 +0100

    fix_tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,30 +1,36 @@
-import {expect} from 'chai'
+import { expect } from "chai";
 import Tetromino from "../src/Tetromino";
 //import Block from "../src/Block";
 
-describe('TetrisTests', function() {
-    let b =  new Tetromino();
-    it('rotates', function() {
-        expect(b.straight.shape).to.include.deep.ordered.members([[0, 0, 0, 0],
-                                                                  [1, 1, 1, 1],
-                                                                  [0, 0, 0, 0],
-                                                                  [0, 0, 0, 0]]);
-        b.rotate("right");
-        expect(b.straight.shape).to.include.deep.ordered.members([[0, 0, 1, 0],
-                                                                  [0, 0, 1, 0],
-                                                                  [0, 0, 1, 0],
-                                                                  [0, 0, 1, 0]]);
-        b.rotate("left");
-        b.rotate("left");
-        expect(b.straight.shape).to.include.deep.ordered.members([[0, 1, 0, 0],
-                                                                  [0, 1, 0, 0],
-                                                                  [0, 1, 0, 0],
-                                                                  [0, 1, 0, 0]]);
-        //b.rotate();
-        //expect(b.straight.shape).to.include.deep.ordered.members([[0, 0, 0, 0],
-        //                                                          [0, 0, 0, 0],
-        //                                                          [1, 1, 1, 1],
-        //                                                          [0, 0, 0, 0]]);
-        // console.log(b);
-    })
-})
+describe("TetrisTests", function () {
+  let b = new Tetromino("I");
+  it("rotates", function () {
+    expect(b.shape).to.include.deep.ordered.members([
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+    b.rotate("right");
+    expect(b.shape).to.include.deep.ordered.members([
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 1, 0],
+    ]);
+    b.rotate("left");
+    b.rotate("left");
+    expect(b.shape).to.include.deep.ordered.members([
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+    ]);
+    //b.rotate();
+    //expect(b.straight.shape).to.include.deep.ordered.members([[0, 0, 0, 0],
+    //                                                          [0, 0, 0, 0],
+    //                                                          [1, 1, 1, 1],
+    //                                                          [0, 0, 0, 0]]);
+    // console.log(b);
+  });
+});
